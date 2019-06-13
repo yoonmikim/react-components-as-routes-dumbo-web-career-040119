cb40f07720be8f31377d28cd0f526a603098f0c7
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-13 20:22:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
 const Home = () => {
   return (
     <div>
@@ -8,8 +10,11 @@ const Home = () => {
     </div>
   );
 };
-
-ReactDOM.render(
-  <Home />,
+ 
+// Step 2. Changed to have router coordinate what is displayed
+ReactDOM.render((
+  <Router>
+    <Route path="/" render={Home} />
+  </Router>),
   document.getElementById('root')
 );

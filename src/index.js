4e57b8a5c03b69a13cac654fbf20d42b8b15a092
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-13 20:23:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -19,6 +19,24 @@ const Home = () => {
   );
 };
  
+ const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+ 
  
 // Step 2. Changed to have router coordinate what is displayed
 ReactDOM.render((
